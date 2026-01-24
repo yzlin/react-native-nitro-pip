5b2bde2ebca0c17eb066822c266b018a5ce5ba63
Ethan Lin <yzlin1985@gmail.com>
2026-01-24 23:53:09 +0800

    chore(ios): add PiP listener registry

--- ios/NitroPip.swift
+++ ios/NitroPip.swift
@@ -1,4 +1,7 @@
 class NitroPip: HybridNitroPipSpec {
+    private var pipModeListenerId: Double = 0
+    private var pipModeListeners: [Double: (Bool) -> Void] = [:]
+
     public func enterPiP(options: EnterPiPOptions?) -> Bool {
         return false
     }
@@ -9,5 +12,16 @@ class NitroPip: HybridNitroPipSpec {
 
     public func isInPiP() -> Bool {
         return false
+    }
+
+    public func addPiPModeChangedListener(listener: @escaping (Bool) -> Void) -> Double {
+        let id = pipModeListenerId
+        pipModeListenerId += 1
+        pipModeListeners[id] = listener
+        return id
+    }
+
+    public func removePiPModeChangedListener(id: Double) {
+        pipModeListeners.removeValue(forKey: id)
     }
 }
